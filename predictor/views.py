4812a6ae50659b18c12be20f885ccb9f4fb810db
TMats <t.matsu.changmin@gmail.com>
2017-01-16 01:42:58 +0900

    fix

--- predictor/views.py
+++ predictor/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+<<<<<<< HEAD
 from django.http import HttpResponse, JsonResponse, Http404
 
 from django.shortcuts import render
+=======
+from django.http import HttpResponse
+
+from django.shortcuts import render_to_response
+>>>>>>> f4dbfcf... [WIP] upload images
 from django.template import RequestContext
 from predictor.models import ImageFile
 
@@ -12,14 +18,20 @@ def index(request):
     return HttpResponse('index')
 
 
+<<<<<<< HEAD
 def upload(request):
     if request.method == 'POST':
         foods = []
+=======
+def upload_form(request):
+    if request.method == 'POST':
+>>>>>>> f4dbfcf... [WIP] upload images
         post_data = request.POST
         post_data.update(request.FILES)
         imagefile = ImageFile()
         image = post_data['image_file']
         if image:
+<<<<<<< HEAD
             imagefile.data = image
             # for debug
             foods.append({'name': u'パイナップル'})
@@ -34,3 +46,8 @@ def upload(request):
 
 def upload_form(request):
     return render(request, 'upload_form.html')
+=======
+            filename = image['filename']
+            imagefile.data = filename
+            imagefile.save(filename)
+>>>>>>> f4dbfcf... [WIP] upload images
