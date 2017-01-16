61dbbe3677f900f27c9ad3c3655ba6fc853f0286
TMats <t.matsu.changmin@gmail.com>
2017-01-16 09:31:31 +0900

    urlまわり

--- predictor/views.py
+++ predictor/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 from predictor.models import ImageFile
 
@@ -12,7 +12,9 @@ def index(request):
     return HttpResponse('index')
 
 
-def upload_form(request):
+def upload(request):
+    foods =[]
+
     if request.method == 'POST':
         post_data = request.POST
         post_data.update(request.FILES)
@@ -22,3 +24,12 @@ def upload_form(request):
             filename = image['filename']
             imagefile.data = filename
             imagefile.save(filename)
+
+    # json response
+    foods.append({'name': u'にんじん'})
+    foods.append({'name': u'じゃがいも'})
+    return JsonResponse({"foods": foods})
+
+
+def upload_form(request):
+    return render(request, 'upload_form.html')
