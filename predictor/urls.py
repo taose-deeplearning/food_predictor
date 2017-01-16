c8b6ceb29a3b9e80e2500ad2d59c65644aebf78f
TMats <t.matsu.changmin@gmail.com>
2017-01-16 18:14:00 +0900

    fix

--- predictor/urls.py
+++ predictor/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^upload/', views.upload, name='upload'),
+    url(r'^upload_form/', views.upload_form, name='upload_form'),
 ]
--- predictor/views.py
+++ predictor/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
 from django.http import HttpResponse, JsonResponse, Http404
 
 from django.shortcuts import render
-=======
-from django.http import HttpResponse
-
-from django.shortcuts import render_to_response
->>>>>>> f4dbfcf... [WIP] upload images
-from django.template import RequestContext
 from predictor.models import ImageFile
 
 
@@ -18,20 +11,14 @@ def index(request):
     return HttpResponse('index')
 
 
-<<<<<<< HEAD
 def upload(request):
     if request.method == 'POST':
         foods = []
-=======
-def upload_form(request):
-    if request.method == 'POST':
->>>>>>> f4dbfcf... [WIP] upload images
         post_data = request.POST
         post_data.update(request.FILES)
         imagefile = ImageFile()
         image = post_data['image_file']
         if image:
-<<<<<<< HEAD
             imagefile.data = image
             # for debug
             foods.append({'name': u'パイナップル'})
@@ -46,8 +33,3 @@ def upload_form(request):
 
 def upload_form(request):
     return render(request, 'upload_form.html')
-=======
-            filename = image['filename']
-            imagefile.data = filename
-            imagefile.save(filename)
->>>>>>> f4dbfcf... [WIP] upload images
