f02764e7eca571b9a59e7f301de9398a81a8e6eb
TMats <t.matsu.changmin@gmail.com>
2017-01-17 16:17:14 +0900

    modify setting

--- predict_food/settings.py
+++ predict_food/settings.py
@@ -123,8 +123,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # settings for images
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
