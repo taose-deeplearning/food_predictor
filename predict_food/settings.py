c4b7304a98b2527579ff531f999023244a57fcee
TMats <t.matsu.changmin@gmail.com>
2017-01-17 15:43:05 +0900

    fix settings

--- predict_food/settings.py
+++ predict_food/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'predict_food',
-        'USER': '',
+        'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
