f3b2036d4e44ad52dcd335b79eab396816d8d569
TMats <t.matsu.changmin@gmail.com>
2017-01-17 16:05:58 +0900

    add static

--- predict_food/settings.py
+++ predict_food/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # settings for images
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
