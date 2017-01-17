ee147198d0d97f66b6840db5e9d4d55facf5e68b
TMats <t.matsu.changmin@gmail.com>
2017-01-17 17:45:02 +0900

    add allowed hosts

--- predict_food/settings.py
+++ predict_food/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2%q37%b9cm88)9@%vmas-@hnndb1o$mmc=*n!c@g%otvcp9(h@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'predict-food-2017.herokuapp.com']
 
 
 # Application definition
